0c23a9c71d15802f3f5d823e0124328d61ffc60e
Davide Bertola <dade@dadeb.it>
2017-01-31 09:31:44 +0100

    make it work with plain css files

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
 module.exports = {
   webpack: (config, { dev }) => {
     config.module.rules.push(
-      { test: /\.css/, loader: "raw-loader" }
+      {
+        test: /\.css/,
+        loader: "emit-file-loader",
+        options: {
+          name: 'dist/[path][name].[ext]'
+        }
+      }
+    ,
+      {
+        test: /\.css/,
+        loader: "babel-loader!raw-loader"
+      }
     )
+    // console.log(config.module.rules)
     return config
   }
 }
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import fetch from 'isomorphic-fetch'
 
 import style from './style.css'
-console.log('style', style)
 
 export default () =>
   <div>
