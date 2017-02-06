3400146813c11ae9f36719748b042fc904f4ebf1
Davide Bertola <dade@dadeb.it>
2017-02-06 19:19:55 +0100

    example style from a nodde module

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
 import React from 'react'
 
+import normalize from '../node_modules/normalize.css/normalize.css'
 import style from './style.scss'
+
+
+
 // or, if you work with plain css
 // import style from './style.css'
 
 export default () =>
   <div>
-    <style>{ style }</style>
+    <style>
+      { normalize }
+      { style }
+    </style>
     <p>ciao</p>
   </div>
